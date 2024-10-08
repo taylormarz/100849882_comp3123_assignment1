ba92d0439513d66bb4bcff9dc5517fd6a567fe9f
taylormarz <taylormartind@gmail.com>
2024-10-08 17:00:32 -0400

    route directory containing RESTful api endpoints for users and employee modules

---
+++ routes/employee-route.js
@@ -0,0 +1,79 @@
+// routes for employee API endpoints
+import { validateEmp, validationErr } from '../handlers/validation-handler.js';
+import { errHandle } from '../handlers/error-handler.js';
+import { createEmployee, readAllEmployees, readEmpById, 
+    updateEmployee, deleteEmployee } from "../modules/employee.js";
+
+// route for post request to create new employee
+export async function createNewEmpRoute(app, employeeCollection) {
+    app.post('/api/v1/emp/employees', validateEmp(), validationErr, async (req, res) => {
+        const { first_name, last_name, email, position, salary, department } = req.body;
+
+        try {
+            const createEmp = await createEmployee(first_name, last_name, email, position, salary, 
+                department, employeeCollection);
+                res.status(201).json({ 
+                    message: 'Employee created successfully.',
+                    employee_id: createEmp._id
+                });
+        } catch (e) {
+            errHandle(null, 'Error creating new employee: ' + e);
+        }
+    });
+}
+
+// route for get request to read all employees
+export async function readEmpRoute(app, employeeCollection) {
+    app.get('/api/v1/emp/employees', async (_, res) => {
+
+        try {
+            const getEmp = (await readAllEmployees(employeeCollection));
+            res.status(200).json({ getEmp });
+        } catch (e) {
+            errHandle(null, 'Error reading employees: ' + e);
+        }
+    });
+}
+
+// route for get request to read employee by id
+export async function readEmpByIdRoute(app, employeeCollection) {
+    app.get('/api/v1/emp/employees/:eid', async (req, res) => {
+        const { eid } = req.params;
+
+        try {
+            const getEmpById = await readEmpById(eid, employeeCollection);
+            res.status(200).json({ getEmpById });
+        } catch (e) {
+            errHandle(null, 'Error reading employee by id: ' + e);
+        }
+    });
+}
+
+// route for updating employee
+export async function updateEmpRoute(app, employeeCollection) {
+    app.put('/api/v1/emp/employees/:eid', validateEmp(), validationErr, async (req, res) => {
+        const { eid } = req.params;
+        const { first_name, last_name, email, position, salary, department } = req.body;
+
+        try {
+            const updateEmpById = await updateEmployee(eid, first_name, last_name, email, 
+                position, salary, department, employeeCollection);
+            res.status(200).json({ message: 'Employee details updated successfully. ', updateEmpById });
+        } catch (e) {
+            errHandle(null, 'Error updating employee: ' + e);
+        }
+    });
+}
+
+export async function deleteEmpRoute(app, employeeCollection) {
+    app.delete('/api/v1/emp/employees/:eid', async (req, res) => {
+        const { eid } = req.params;
+
+        try {
+            const deleteEmp = await deleteEmployee(eid, employeeCollection);
+            res.status(204).json({ message: 'Employee deleted successfully. ', deleteEmp })
+        } catch (e) {
+            errHandle(null, 'Error deleting employee: ' + e);
+        }
+    })
+}
---
+++ routes/user-route.js
@@ -0,0 +1,36 @@
+// routes for user API endpoints
+import { validateUser, validationErr } from '../handlers/validation-handler.js';
+import { errHandle } from '../handlers/error-handler.js';
+import { createUser, userLogin } from '../modules/user.js';
+
+// route for post request to create new user
+export async function createUserRoute(app, userCollection) {
+    app.post('/api/v1/user/signup', validateUser(), validationErr, async (req, res) => {
+
+        const { username, email, password } = req.body;
+
+        try {
+            const create = await createUser(username, email, password, userCollection);
+            res.status(201).json({ 
+                message: 'User created successfully. ',
+                user_id: create._id
+            });
+        } catch (e) {
+            errHandle(null, 'Error creating new user: ' + e);
+        }
+    });
+}
+
+// route to verify user login credentials
+export async function verifyUserRoute(app, userCollection) {
+    app.post('/api/v1/user/login', async (req, res) => {
+        const { email_username, password } = req.body;
+
+        try {
+            await userLogin(email_username, password, userCollection);
+            res.status(200).json({ message: 'Login successful.' });
+        } catch (e) {
+            errHandle(null, 'Error verifying user: ' + e);
+        }
+    });
+}
