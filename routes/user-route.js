ab762680930d05f6a8af19a56fb284579e1070c7
taylormarz <taylormartind@gmail.com>
2024-10-11 15:00:15 -0400

    added extra comments in user.js for clarity

--- routes/user-route.js
+++ routes/user-route.js
@@ -6,16 +6,19 @@ import { createUser, userLogin } from '../modules/user.js';
 // route for post request to create new user
 export async function createUserRoute(app, userCollection) {
     app.post('/api/v1/user/signup', validateUser(), validationErr, async (req, res) => {
-
+        // destructure user details from req body
         const { username, email, password } = req.body;
 
         try {
+            // call createUser func from user.js
             const create = await createUser(username, email, password, userCollection);
+            // success response
             res.status(201).json({ 
                 message: 'User created successfully. ',
                 user_id: create._id
             });
         } catch (e) {
+            // err handler func incase error occurs
             errHandle(null, 'Error creating new user: ' + e);
         }
     });
@@ -24,12 +27,16 @@ export async function createUserRoute(app, userCollection) {
 // route to verify user login credentials
 export async function verifyUserRoute(app, userCollection) {
     app.post('/api/v1/user/login', async (req, res) => {
+        // destructure user details from req body
         const { email_username, password } = req.body;
 
         try {
+            // call userLogin func from user.js
             await userLogin(email_username, password, userCollection);
+            // success response
             res.status(200).json({ message: 'Login successful.' });
         } catch (e) {
+            // returns error status and message if login fails
             return res.status(401).json({ message: 'Login failed.' });
         }
     });
